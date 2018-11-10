b24f5cae3cbcc23978fad139554b746d6a75af56
DanoCode01 <39469800+DanoCode01@users.noreply.github.com>
2018-11-10 13:02:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+// GET INPUT ELEMENT
+let filterInput = document.getElementById('filterInput');
+// ADD EVENT LISTENER
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames() {
+    // Get value of input
+
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    console.log(filterValue);
+
+    // Get names ul 
+    let ul = document.getElementById('names');
+    // Get li's from ul
+    let li = ul.querySelectorAll('li.collection-item');
+
+    //Loop through all collection-item li's
+    for (var i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName('a')[0];
+        // If matched
+
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            li[i].style.display = '';
+            
+        } else {
+            li[i].style.display = 'none';
+            
+        }
+        
+    }
+}
